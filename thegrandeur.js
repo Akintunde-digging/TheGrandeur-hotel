0a3e57fe9c447a5b1c28c2be33dd49a51702a3f1
Akintunde-digging <isaacakintunde11@gmail.com>
2026-01-30 16:26:16 +0100

    Add files via upload

---
+++ thegrandeur.js
@@ -0,0 +1,23 @@
+const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('navMenu');
+    
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+
+        if (navMenu.classList.contains('active')) {
+            hamburger.classList.remove('fa-bars');
+            hamburger.classList.add('fa-times');
+        } else {
+            hamburger.classList.remove('fa-times');
+            hamburger.classList.add('fa-bars');
+        }
+    });
+
+    const navLinks = document.querySelectorAll('.home-contact a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+            hamburger.classList.remove('fa-times');
+            hamburger.classList.add('fa-bars');
+        });
+    });
